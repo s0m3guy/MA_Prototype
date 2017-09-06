0e7cba70f4a7cb1a0f9a85672fe648d3c4263a48
Unknown <someguy555@gmail.com>
2017-09-06 16:49:18 +0200

    Drew a line between test blocks
    Created a line renderer connected between input/output of testing blocks

--- MA_Prototype/Assets/Collision_Script.cs
+++ MA_Prototype/Assets/Collision_Script.cs
@@ -15,7 +15,8 @@ public class Collision_Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
---
+++ MA_Prototype/Assets/Draw_Line.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_Line : MonoBehaviour {
+
+	private LineRenderer lineRenderer = new LineRenderer();
+
+	public Transform origin;
+	public Transform destin;
+
+	void Awake () {
+
+		lineRenderer = GetComponent<LineRenderer> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+//		line.transform.localScale += new Vector3 (0.1F, 0, 0);
+//		lineRenderer.SetPosition(0, new Vector3 (40,50,0));
+
+//		transform.localScale += new Vector3 (0.1F, 0, 0);
+
+//		lineRenderer.SetPosition (1, new Vector3(transform.position.x, transform.position.y ,0));
+//		lineRenderer.SetPosition (1, GameObject.FindGameObjectWithTag("outputA").transform.position);
+//		lineRenderer.SetPosition (0, GameObject.FindGameObjectWithTag ("inputB2").transform.position);
+		//		lineRenderer.SetPosition (1, new Vector3(2,4,0));
+
+		lineRenderer.SetPosition (0, origin.position);
+		lineRenderer.SetPosition (1, destin.position);
+
+	}
+}
