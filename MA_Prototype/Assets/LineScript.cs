126ba36791358581e04990173ec0f74d0aa23ae7
Unknown <someguy555@gmail.com>
2017-10-09 16:42:07 +0200

    Changed input colliders from box to circle collider

--- MA_Prototype/Assets/LineScript.cs
+++ MA_Prototype/Assets/LineScript.cs
@@ -6,15 +6,15 @@ public class LineScript : MonoBehaviour {
 
 	private LineRenderer line = new LineRenderer();
 	private GameObject goalInput;
-	private BoxCollider2D boxCol;
+	private CircleCollider2D circCol;
 	private Rect goalBox;
 	private Transform origin, destin;
 
 
 	void Awake () {
-		goalInput = GameObject.FindGameObjectWithTag("inputB1");
+		goalInput = GameObject.FindGameObjectWithTag("inputB2");
 		line = GetComponent<LineRenderer> ();
-		boxCol = goalInput.GetComponent<BoxCollider2D> ();
+		circCol = goalInput.GetComponent<CircleCollider2D> ();
 
 		origin = GameObject.FindGameObjectWithTag ("outputA").transform;
 		destin = goalInput.transform;
@@ -32,7 +32,8 @@ public class LineScript : MonoBehaviour {
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		mousePos.z = 0;
 
-		if (boxCol.bounds.Contains(mousePos)) {
+		if (circCol.bounds.Contains(mousePos)) {
+				
 			line.SetPosition (1, new Vector3(
 				destin.position.x - (destin.GetComponent<SpriteRenderer>().bounds.size.x)/2,
 				destin.position.y,
