3979cc92b2fde0df17a2fb7b62631137199cb2bf
Unknown <someguy555@gmail.com>
2017-12-13 17:50:01 +0100

    Created interface "Conductive"

---
+++ MA_Prototype/Assets/Conductive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductive : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
