7e29de944835d0dba601a395709f51993c20a51c
Unknown <someguy555@gmail.com>
2018-04-01 00:42:23 +0200

    Value FB now also has gradient input/output

--- MA_Prototype/Assets/FunctionBlock.cs
+++ MA_Prototype/Assets/FunctionBlock.cs
@@ -28,6 +28,8 @@ public class FunctionBlock : MonoBehaviour {
 
 	bool elementAboveWasteBin = false;
 
+	Color lerpedColor;
+
 	void Awake () {
 
 		inputs = new float[transform.childCount - 2];		// Total amount minus canvas+output equals the amount of inputs
@@ -53,9 +55,12 @@ public class FunctionBlock : MonoBehaviour {
 	void Update () {
 		forwardInput ();
 
-		if (inputs [0] == 0) {
+		if (transform.parent.name.Contains ("VALUE")) {
+			lerpedColor = Color.Lerp (Color.white, Color.green, inputs [0] / 5);
+			input1GO.GetComponent<SpriteRenderer> ().color = lerpedColor;
+		} else if (!transform.parent.name.Contains("VALUE") && inputs [0] == 0) {
 			input1GO.GetComponent<SpriteRenderer> ().color = Color.white;
-		} else if (inputs [0] == 1) {
+		} else if (!transform.parent.name.Contains("VALUE") && inputs [0] == 1) {
 			input1GO.GetComponent<SpriteRenderer> ().color = Color.green;
 		}
 
@@ -65,35 +70,39 @@ public class FunctionBlock : MonoBehaviour {
 			input2GO.GetComponent<SpriteRenderer> ().color = Color.green;
 		}
 
-		if (output == 0) {
-			outputGO.GetComponent<SpriteRenderer> ().color = Color.white;
-		} else if (output == 1) {
-			outputGO.GetComponent<SpriteRenderer> ().color = Color.green;
-		}
-
-		// in case removal overlay is active
-		if (testSquare != null && testSquare.activeSelf) {
-			this.GetComponent<SpriteRenderer> ().sortingLayerName = "Line";
-			this.GetComponentInChildren<Canvas> ().sortingLayerName = "Waste Bin On Overlay";
-			this.GetComponentInChildren<Canvas> ().sortingOrder = 1;
-
-			foreach (SpriteRenderer spriteRend in GetComponentsInChildren<SpriteRenderer>()) {
-				spriteRend.sortingLayerName = "Waste Bin On Overlay";
+		if (transform.parent.name.Contains ("VALUE")) {
+			lerpedColor = Color.Lerp (Color.white, Color.green, output / 5);
+			outputGO.GetComponent<SpriteRenderer> ().color = lerpedColor;
+			if (!transform.parent.name.Contains ("VALUE") && output == 0) {
+				outputGO.GetComponent<SpriteRenderer> ().color = Color.white;
+			} else if (!transform.parent.name.Contains ("VALUE") && output == 1) {
+				outputGO.GetComponent<SpriteRenderer> ().color = Color.green;
 			}
 
-		// in case it's not active
-		} else if (testSquare != null && !testSquare.activeSelf) {
-			testSquare.SetActive (false);
-			this.GetComponent<SpriteRenderer> ().sortingLayerName = "Functional Blocks";
-			this.GetComponentInChildren<Canvas> ().sortingLayerName = "FB Label";
-		}
+			// in case removal overlay is active
+			if (testSquare != null && testSquare.activeSelf) {
+				this.GetComponent<SpriteRenderer> ().sortingLayerName = "Line";
+				this.GetComponentInChildren<Canvas> ().sortingLayerName = "Waste Bin On Overlay";
+				this.GetComponentInChildren<Canvas> ().sortingOrder = 1;
 
-		// In case no line is connected to the inputs, reset input values
-		if (!GetComponentInChildren<InputCircle> ().connectedLine) {
-			foreach (int element in inputs) {
-				inputs [element] = 0;
+				foreach (SpriteRenderer spriteRend in GetComponentsInChildren<SpriteRenderer>()) {
+					spriteRend.sortingLayerName = "Waste Bin On Overlay";
+				}
+
+				// in case it's not active
+			} else if (testSquare != null && !testSquare.activeSelf) {
+				testSquare.SetActive (false);
+				this.GetComponent<SpriteRenderer> ().sortingLayerName = "Functional Blocks";
+				this.GetComponentInChildren<Canvas> ().sortingLayerName = "FB Label";
 			}
-			output = 0;
+
+			// In case no line is connected to the inputs, reset input values
+			if (!GetComponentInChildren<InputCircle> ().connectedLine) {
+				foreach (int element in inputs) {
+					inputs [element] = 0;
+				}
+				output = 0;
+			}
 		}
 	}
 
