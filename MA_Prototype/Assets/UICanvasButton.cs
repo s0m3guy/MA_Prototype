4e6ad16b2cc35ffb15da48d7b5acbeb8396ad77b
Unknown <someguy555@gmail.com>
2018-04-05 14:42:54 +0200

    Created class for button which features output of UI canvas data

---
+++ MA_Prototype/Assets/UICanvasButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICanvasButton : MonoBehaviour {
+
+	public Button yourButton;
+	public GameObject chosenDropdownEntry;
+	public GameObject userInputValue;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+		Debug.Log(chosenDropdownEntry.GetComponent<Text>().text);
+		Debug.Log(userInputValue.GetComponent<Text>().text);
+	}
+}
