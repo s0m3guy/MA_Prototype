7d4929d8044bbea54d3449882588f69407478906
Unknown <someguy555@gmail.com>
2018-04-12 14:32:31 +0200

    Adjusted breadboards left and right and added gears with script

---
+++ MA_Prototype/Assets/RotateGear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGear : MonoBehaviour {
+
+	public GameObject gear1;
+	public GameObject gear2;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gear1.transform.Rotate(0, 0, speed);
+		gear2.transform.Rotate(0, 0, speed*-1);
+	}
+}
